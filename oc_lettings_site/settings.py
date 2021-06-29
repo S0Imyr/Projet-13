f98ddbecbb9d4128414b8fdc6724c08688ffd62c
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-06-29 14:38:12 +0200

    Delete oc_letting_site files

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profiles.apps.ProfilesConfig',
+    'lettings.apps.LettingsConfig',
 ]
 
 MIDDLEWARE = [
