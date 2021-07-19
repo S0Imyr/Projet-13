2655cd2adc099e201f89515aaaefd45578f43267
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-07-19 14:31:51 +0200

    Configure Profile Tests

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,16 +1,29 @@
-from django.test import TestCase, Client
+from django.test import Client
+from django.urls.base import reverse
+
+import pytest
+
+from .models import User, Profile
 
 
 class Test:
     def setup_method(self):
         self.client = Client()
+        self.user = User.objects.create(username="test_user")
+        self.profile = Profile.objects.create(user=self.user, favorite_city="Charleville Mézières")
 
     def teardown_method(self):
         pass
 
+    @pytest.mark.django_db
     def test_profiles_index(self):
-        pass
+        uri = reverse('profiles_index')
+        response = self.client.get(uri)
+        assert response.status_code == 200
 
+    @pytest.mark.django_db
     def test_profiles(self):
-        pass
+        uri = reverse('profile', args=[self.profile.user.username])
+        response = self.client.get(uri)
+        assert response.status_code == 200
 
